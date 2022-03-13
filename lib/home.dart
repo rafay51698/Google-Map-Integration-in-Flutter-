edf89f3b60558aa939cceef8deb4e5ea03253644
rafay51698 <rafay516982gmail.com>
2022-03-13 20:53:54 +0500

    Final

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Address extends StatefulWidget {
+  const Address({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Address> createState() => _AddressState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _AddressState extends State<Address> {
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(24.937182, 67.147488),
     zoom: 14.4746,
   );
+
+  final List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(24.937182, 67.147488),
+        infoWindow: InfoWindow(title: 'My current Location')),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: GoogleMap(
         initialCameraPosition: _kGooglePlex,
         mapType: MapType.normal,
         myLocationEnabled: true,
+        markers: Set<Marker>.of(_marker),
       ),
     );
   }
