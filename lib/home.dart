1a6d9a5dc013e603af675a294da3836bdfa502a5
rafay51698 <rafay516982gmail.com>
2022-03-14 23:32:40 +0500

    Final Live tracking

--- lib/home.dart
+++ lib/home.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Address extends StatefulWidget {
-  const Address({Key? key}) : super(key: key);
+import 'screens/current_location_screen.dart';
+import 'screens/simple_map_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<Address> createState() => _AddressState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _AddressState extends State<Address> {
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(24.937182, 67.147488),
-    zoom: 14.4746,
-  );
-
-  final List<Marker> _marker = [];
-  final List<Marker> _list = const [
-    Marker(
-        markerId: MarkerId('1'),
-        position: LatLng(24.937182, 67.147488),
-        infoWindow: InfoWindow(title: 'My current Location')),
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _marker.addAll(_list);
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        mapType: MapType.normal,
-        myLocationEnabled: true,
-        markers: Set<Marker>.of(_marker),
+      appBar: AppBar(
+        title: const Text("Flutter Google Maps"),
+        centerTitle: true,
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const SimpleMapScreen();
+                  }));
+                },
+                child: const Text("Simple Map")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const CurrentLocationScreen();
+                  }));
+                },
+                child: const Text("User current location"))
+          ],
+        ),
       ),
     );
   }
